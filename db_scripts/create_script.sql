9a9e2550fe828e3e39224ed86088de8324252c05
YannikBeimler <yannik.beimler@students.fhnw.ch>
2020-10-09 19:53:16 +0200

    correct decimal size for latitude and longitude in address

--- db_scripts/create_script.sql
+++ db_scripts/create_script.sql
@@ -22,8 +22,8 @@ CREATE TABLE tblAddress (
     intAddressPK int IDENTITY(1,1) NOT NULL,
     strStreet nvarchar(255) NOT NULL,
     strCity nvarchar(255) NOT NULL,
-    decLatitude decimal(3,6)  NOT NULL,
-    decLongitude decimal(3,6)  NOT NULL,
+    decLatitude decimal(11, 8)  NOT NULL,
+    decLongitude decimal(11, 8)  NOT NULL,
     PRIMARY KEY (intAddressPK)
 );
 
