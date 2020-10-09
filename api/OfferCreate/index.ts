d9a5e4b0dd405141a2332580b638738dad8b5655
YannikBeimler <yannik.beimler@students.fhnw.ch>
2020-10-09 23:54:23 +0200

    save offers

--- api/OfferCreate/index.ts
+++ api/OfferCreate/index.ts
@@ -5,9 +5,7 @@ import DbService from "../Db/DbService";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log("HTTP trigger function processed a request.");
   const offer: Offer = req.body as Offer;
-  const responseMessage = `gameId: ${offer.gameId}\nuserId: ${offer.userId}\naddressId: ${offer.addressId}`;
-
-  //const response = DbService
+  const responseMessage = await DbService.addOffer(offer);
 
   context.res = {
     status: 201 /* Defaults to 200 */,
