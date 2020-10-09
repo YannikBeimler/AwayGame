4692ef444544e77a2fac20394354a040313cca20
Humbi <svenhumbel@students.fhnw.ch>
2020-10-09 18:37:04 +0200

    fixed styling

--- src/view/games/games.tsx
+++ src/view/games/games.tsx
@@ -9,24 +9,17 @@ export function Games() {
       <Container
         className={"container-fluid games-container"}
         style={{ backgroundColor: "#232323", height: "100%", overflowY: "auto" }}>
-        <Row className={"match-box"}>
-          <Col className={"match-box-content"}>
-            <p className={"match-box-content-date text-center"}>Sa. 17. Oktober 2020</p>
-            <div className={"match-box-content-divider text-center"} />
-            <p className={"match-box-content-time text-center"}>19:00 Uhr</p>
-            <img className={"logo-team-one"} src={"assets/images/clubs/servettefc.png"} alt={"Logo Team 1"} />
-            <img className={"logo-team-two"} src={"assets/images/clubs/yb.png"} alt={"Logo Team 2"} />
-          </Col>
-        </Row>
-        <Row className={"match-box"}>
-          <Col className={"match-box-content"}>
-            <p className={"match-box-content-date text-center"}>Sa. 17. Oktober 2020</p>
-            <div className={"match-box-content-divider text-center"} />
-            <p className={"match-box-content-time text-center"}>19:00 Uhr</p>
-            <img className={"logo-team-one"} src={"assets/images/clubs/yb.png"} alt={"Logo Team 1"} />
-            <img className={"logo-team-two"} src={"assets/images/clubs/fcluzern.png"} alt={"Logo Team 2"} />
-          </Col>
-        </Row>
+        <a href={"/games/"}>
+          <Row className={"match-box"}>
+            <Col className={"match-box-content"}>
+              <p className={"match-box-content-date text-center"}>Sa. 17. Oktober 2020</p>
+              <div className={"match-box-content-divider text-center"} />
+              <p className={"match-box-content-time text-center"}>19:00 Uhr</p>
+              <img className={"logo-team-one"} src={"assets/images/clubs/servettefc.png"} alt={"Logo Team 1"} />
+              <img className={"logo-team-two"} src={"assets/images/clubs/yb.png"} alt={"Logo Team 2"} />
+            </Col>
+          </Row>
+        </a>
       </Container>
     </>
   );
