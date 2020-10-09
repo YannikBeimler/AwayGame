e03be71f9502edf92536850dca6cca893cb9ba43
mauro <maurostehle@gmail.com>
2020-10-09 17:11:46 +0200

    updated wrong routes

--- api/OfferById/index.ts
+++ api/OfferById/index.ts
@@ -3,8 +3,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
-  const id = context.bindingData.id;
-  const responseMessage = `You got the game with the id ${id}`;
+  const gameId = context.bindingData.gameId;
+  const offerId = context.bindingData.offerId;
+  const responseMessage = `You got the offer with the id ${offerId} from the game with the id ${gameId}`;
 
   context.res = {
     // status: 200, /* Defaults to 200 */
